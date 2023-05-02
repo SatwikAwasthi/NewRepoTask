f455ee3f506e8bfd4d66578cf533f794738788af
SatwikAwasthi <70152444+SatwikAwasthi@users.noreply.github.com>
2023-05-02 18:32:55 +0530

    Add files via upload

---
+++ BankingAPI/BankingAPI/Controllers/AccountController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BankingAPI.Models;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BankingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        Account _objAcc = new Account();
+        // GET: api/values
+        [HttpGet]
+        [Route("account/all")]
+        public IActionResult GetAllAcccounts()
+        {
+            try
+            {
+                return Ok(_objAcc.DisplayAllAccounts());
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("account/byType/{type}")]
+        public IActionResult GetAllByAccType(string type)
+        {
+            try
+            {
+                return Ok(_objAcc.DisplayAllAccountsByType(type));
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("account/byStatus/{status}")]
+        public IActionResult GetAllByAccStatus(bool status)
+        {
+            try
+            {
+                return Ok(_objAcc.DisplayAllAccountsByStatus(status));
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("account/byNumber/{num}")]
+        public IActionResult GetByAccNumber(int num)
+        {
+            try
+            {
+                return Ok(_objAcc.DisplayAcountByID(num));
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        [Route("account/add/{AccNum}/{AccName}/{AccType}/{AccBalance}/{AccIsActive}")]
+        public IActionResult AddNewAccount(Account acc)
+        {
+            try
+            {
+                return Created("", _objAcc.AddNewAccount(acc));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        [Route("account/edit/{AccNum}/{AccName}/{AccType}/{AccBalance}/{AccIsActive}")]
+        public IActionResult EditAccount(Account acc)
+        {
+            try
+            {
+                return Ok(_objAcc.EditAccountDetail(acc));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/values/5
+        [HttpDelete]
+        [Route("account/delete/{id}")]
+        public IActionResult DeleteAccount(int id)
+        {
+            try
+            {
+                return Ok(_objAcc.DeleteAccount(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
+
---
+++ BankingAPI/BankingAPI/Models/Account.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using static BankingAPI.Models.Account;
+
+namespace BankingAPI.Models
+{
+	public class Account
+	{
+        #region Properties
+        public int AccNum { get; set; }
+		public string AccName { get; set; }
+		public string AccType { get; set; }
+		public double AccBalance { get; set; }
+		public bool AccIsActive { get; set; }
+		#endregion Properties
+
+		private static List<Account> lstAccounts = new List<Account>()
+		{
+			new Account(){AccNum = 101,AccName = "Ramesh",AccType = "Savings",AccBalance = 38000,AccIsActive = true},
+			new Account(){AccNum = 102,AccName = "Adam",AccType = "Savings",AccBalance = 5600,AccIsActive = false},
+			new Account(){AccNum = 103,AccName = "Aakash",AccType = "Current",AccBalance = 108000,AccIsActive = false},
+			new Account(){AccNum = 104,AccName = "Supriya",AccType = "PF",AccBalance = 567890,AccIsActive = true},
+			new Account(){AccNum = 105,AccName = "Rohit",AccType ="Current",AccBalance = 24770,AccIsActive = true}
+		};
+
+        #region Get methods
+        public List<Account> DisplayAllAccounts()
+		{
+			if (lstAccounts.Any())
+				return lstAccounts;
+			else
+				throw new Exception("Sorry,No Account Available");
+		}
+
+        public List<Account> DisplayAllAccountsByType(string accounttype)
+        {
+			List<Account> lstac = new List<Account>();
+			lstac = lstAccounts.Where(x => x.AccType == accounttype).ToList();
+
+            if (lstac.Any())
+				return lstac;
+			else
+				throw new Exception("Sorry,No Account Available for given Account type");
+        }
+
+        public List<Account> DisplayAllAccountsByStatus(bool status)
+        {
+            var lstac = lstAccounts.Where(x => x.AccIsActive == status).ToList();
+			if (lstac.Any())
+				return lstac;
+			else
+				throw new Exception("Sorry,No Account Available for given status");
+        }
+        public Account DisplayAcountByID(int id)
+        {
+			var lstac = lstAccounts.Where(x => x.AccNum == id).Select(x => x).FirstOrDefault();
+			if (lstac != null)
+				return lstac;
+			else
+				throw new Exception("Sorry,No Account Available for given Account number");
+        }
+        #endregion Get methods
+
+        public string AddNewAccount(Account acc)
+        {
+			if (!lstAccounts.Any(x => x.AccNum == acc.AccNum))
+			{
+				lstAccounts.Add(acc);
+				return "Account Added successfully";
+			}
+			else
+				throw new Exception("Sorry, Account number already exists.");
+        }
+
+        public string EditAccountDetail(Account acc)
+        {
+            var acnt = lstAccounts.Where(x => x.AccNum == acc.AccNum).Select(x => x).FirstOrDefault();
+			if (acnt != null)
+			{
+				acnt.AccName = acc.AccName;
+				acnt.AccType = acc.AccType;
+				acnt.AccBalance = acc.AccBalance;
+				acnt.AccIsActive = acc.AccIsActive;
+				return "Account Edited successfully";
+			}
+			else
+				throw new Exception("Sorry, Account number does not exists.");
+        }
+
+        public string DeleteAccount(int accnmbr)
+        {
+            var acnt = lstAccounts.Where(x => x.AccNum == accnmbr).Select(x => x).FirstOrDefault();
+			if (acnt != null)
+			{
+				lstAccounts.Remove(acnt);
+				return "Account Deleted successfully";
+			}
+			else
+				throw new Exception("Sorry, Account number does not exists.");
+        }
+    }
+}
+
